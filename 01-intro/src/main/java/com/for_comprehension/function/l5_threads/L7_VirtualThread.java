f31788a3534de2af3a60e79fffefebea298f55a5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-08-03 14:58:16 +0200

    Fix

--- 01-intro/src/main/java/com/for_comprehension/function/l5_threads/L7_VirtualThread.java
+++ 01-intro/src/main/java/com/for_comprehension/function/l5_threads/L7_VirtualThread.java
@@ -1,11 +1,8 @@
 package com.for_comprehension.function.l5_threads;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class L7_VirtualThread {
 
-    public static void main(String[] args) throws InterruptedException {
+    /*public static void main(String[] args) throws InterruptedException {
         try (ExecutorService e = Executors.newVirtualThreadPerTaskExecutor()) {
             for (int i = 0; i < 100000; i++) {
                 e.submit(() -> {
@@ -22,22 +19,6 @@ public class L7_VirtualThread {
         }
 
         System.out.println("finished");
-       /* Thread hello_from_virtual_thread = Thread.ofVirtual()
-            .name("fooooooo")
-            .start(() -> {
-            try {
-                Thread.sleep(1000000);
-            }
-            catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            System.out.println("Hello from virtual thread");
-
-        });
-
-        hello_from_virtual_thread.join();*/
-
-    }
+    }*/
 
 }
