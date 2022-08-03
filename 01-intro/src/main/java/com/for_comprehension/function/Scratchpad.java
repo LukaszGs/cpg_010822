32c74f43cb54f7cead8584551ec4b3bb5e3f51f4
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-08-03 13:03:22 +0200

    14. CompletableFuture - overview

--- 01-intro/src/main/java/com/for_comprehension/function/Scratchpad.java
+++ 01-intro/src/main/java/com/for_comprehension/function/Scratchpad.java
@@ -1,30 +1,58 @@
 package com.for_comprehension.function;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Future;
 
 public class Scratchpad {
 
-    private static volatile Long foo = 42L;
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService e = Executors.newFixedThreadPool(4);
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        executorService.submit(() -> {
-            foo = 1L;
+        BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(100);
+        Future<?> submit = e.submit(() -> {
+            queue.add(1);
+        });
+        Future<?> f2 = e.submit(() -> {
+            queue.add(2);
+        });
+        Future<?> f3 = e.submit(() -> {
+            queue.add(3);
         });
 
-        executorService.submit(() -> {
-            while (true) {
-                Thread.sleep(100);
-                System.out.println(foo);
+        Integer integer = queue.take();
+
+        System.out.println(integer);
+
+        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(5000);
             }
+            catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            return 11;
         });
+        CompletableFuture<Integer> cf2 = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(3000);
+            }
+            catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            return 22;
+        });
+
+        cf1
+            .applyToEither(cf2, i -> i)
+            .thenAccept(System.out::println)
+            .join();
+
 
     }
+
 }
