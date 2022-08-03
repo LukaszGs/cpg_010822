0043ebbc8dda8f453189acbba222fc752443a09b
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-08-03 08:37:15 +0200

    12. Stream Kata

--- 01-intro/src/main/java/com/for_comprehension/function/Scratchpad.java
+++ 01-intro/src/main/java/com/for_comprehension/function/Scratchpad.java
@@ -1,7 +1,10 @@
 package com.for_comprehension.function;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,14 +12,11 @@ import java.util.stream.Stream;
 public class Scratchpad {
 
     public static void main(String[] args) {
-        Stream.of(List.of(1), List.of(2, 3))
-            .flatMap(list -> list.stream())
-            .collect(Collectors.toList());
 
-        Stream.of(Optional.of(1),Optional.of(2), Optional.empty())
-            .flatMap(optional -> optional.stream())
-            .collect(Collectors.toList());
+        Map<String, String> foo = Map.of("foo", "bar");
+
+        foo.entrySet().stream()
+            .forEach(System.out::println);
 
     }
-
 }
